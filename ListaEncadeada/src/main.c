c930d68acef6c300318f554178d0a942afb56318
Tiago Alves de Oliveira <tiagofga@gmail.com>
2026-06-04 15:05:18 -0300

    Add singly linked list example

---
+++ ListaEncadeada/src/main.c
@@ -0,0 +1,22 @@
+#include "lista_encadeada.h"
+
+#include <stdio.h>
+
+int main(void) {
+    ListaEncadeada lista;
+    lista_encadeada_criar(&lista);
+
+    lista_encadeada_inserir_inicio(&lista, 30);
+    lista_encadeada_inserir_inicio(&lista, 20);
+    lista_encadeada_inserir_fim(&lista, 40);
+
+    lista_encadeada_imprimir(&lista);
+    printf("Contem 20? %s\n", lista_encadeada_buscar(&lista, 20) ? "sim" : "nao");
+
+    lista_encadeada_remover(&lista, 20);
+    lista_encadeada_imprimir(&lista);
+    printf("%s\n", lista_encadeada_complexidade());
+
+    lista_encadeada_destruir(&lista);
+    return 0;
+}
