6dab12cb72fa9dfb4f84fe12b996093cd85067d5
Tiago Alves de Oliveira <tiagofga@gmail.com>
2026-06-04 15:03:23 -0300

    Add singly linked list header

---
+++ ListaEncadeada/include/lista_encadeada.h
@@ -0,0 +1,26 @@
+#ifndef LISTA_ENCADEADA_H
+#define LISTA_ENCADEADA_H
+
+#include <stddef.h>
+
+typedef struct ListaEncadeadaNo {
+    int valor;
+    struct ListaEncadeadaNo *proximo;
+} ListaEncadeadaNo;
+
+typedef struct {
+    ListaEncadeadaNo *inicio;
+    size_t tamanho;
+} ListaEncadeada;
+
+void lista_encadeada_criar(ListaEncadeada *lista);
+void lista_encadeada_destruir(ListaEncadeada *lista);
+int lista_encadeada_inserir_inicio(ListaEncadeada *lista, int valor);
+int lista_encadeada_inserir_fim(ListaEncadeada *lista, int valor);
+int lista_encadeada_remover(ListaEncadeada *lista, int valor);
+int lista_encadeada_buscar(const ListaEncadeada *lista, int valor);
+size_t lista_encadeada_tamanho(const ListaEncadeada *lista);
+void lista_encadeada_imprimir(const ListaEncadeada *lista);
+const char *lista_encadeada_complexidade(void);
+
+#endif
