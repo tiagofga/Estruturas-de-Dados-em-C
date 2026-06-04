32451e7ecb081f6f75c0543c6434847ac8d89115
Tiago Alves de Oliveira <tiagofga@gmail.com>
2026-06-04 14:20:20 -0300

    Adicionar API de aresta ponderada em grafo

--- Grafo/include/grafo.h
+++ Grafo/include/grafo.h
@@ -13,6 +13,7 @@ int grafo_criar(Grafo *grafo, size_t vertices);
 int grafo_criar_direcionado(Grafo *grafo, size_t vertices, int direcionado);
 void grafo_destruir(Grafo *grafo);
 int grafo_adicionar_aresta(Grafo *grafo, size_t origem, size_t destino);
+int grafo_adicionar_aresta_ponderada(Grafo *grafo, size_t origem, size_t destino, int peso);
 int grafo_remover_aresta(Grafo *grafo, size_t origem, size_t destino);
 int grafo_tem_aresta(const Grafo *grafo, size_t origem, size_t destino);
 int grafo_bfs(const Grafo *grafo, size_t inicio, size_t *ordem, size_t capacidade, size_t *visitados);
